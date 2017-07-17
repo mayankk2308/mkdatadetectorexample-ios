05d8d40793f9f050fab75e9bda12b14ee73dc5d2
Mayank Kumar <mayankk2308@gmail.com>
2017-07-17 16:56:19 +0530

    Add basic UILabel highlighting capability

--- MKDataDetectorExample/WelcomeViewController.swift
+++ MKDataDetectorExample/WelcomeViewController.swift
@@ -7,19 +7,54 @@
 //
 
 import UIKit
+import MKDataDetector
 
 class WelcomeViewController: UIViewController {
-
+    
+    @IBOutlet var eventLabel: UILabel!
+    @IBOutlet var addressLabel: UILabel!
+    @IBOutlet var linkLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        eventLabel.highlightDates()
+        addressLabel.highlightAddresses()
+        linkLabel.highlightLinks()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+extension UILabel {
+    
+    func highlightDates() {
+        let dataDetectorService = MKDataDetectorService()
+        guard let textBody = text else { return }
+        guard let dates = dataDetectorService.extractDates(fromTextBody: textBody) else { return }
+        guard let analysisResult = dates.first else { return }
+        let attributedString = NSMutableAttributedString(string: analysisResult.source)
+        attributedString.addAttribute(NSForegroundColorAttributeName, value: UIColor.purple, range: analysisResult.rangeInSource)
+        attributedText = attributedString
     }
-
-
+    
+    func highlightAddresses() {
+        let dataDetectorService = MKDataDetectorService()
+        guard let textBody = text else { return }
+        guard let dates = dataDetectorService.extractAddresses(fromTextBody: textBody) else { return }
+        guard let analysisResult = dates.first else { return }
+        let attributedString = NSMutableAttributedString(string: analysisResult.source)
+        attributedString.addAttribute(NSForegroundColorAttributeName, value: UIColor.orange, range: analysisResult.rangeInSource)
+        attributedText = attributedString
+    }
+    
+    func highlightLinks() {
+        
+        let dataDetectorService = MKDataDetectorService()
+        guard let textBody = text else { return }
+        guard let dates = dataDetectorService.extractLinks(fromTextBody: textBody) else { return }
+        guard let analysisResult = dates.first else { return }
+        let attributedString = NSMutableAttributedString(string: analysisResult.source)
+        attributedString.addAttribute(NSForegroundColorAttributeName, value: UIColor.blue, range: analysisResult.rangeInSource)
+        attributedText = attributedString
+    }
 }
 
